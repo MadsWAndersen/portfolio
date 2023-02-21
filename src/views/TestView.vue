8ee0d58123ce3c992f152aa95db243e11bd45066
Mads Welling <madswandersen@hotmail.com>
2023-02-21 09:51:34 +0100

    Making an extra route TestView

---
+++ src/views/TestView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">h</div>
+            <div class="col-6">h</div>
+        </div>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  

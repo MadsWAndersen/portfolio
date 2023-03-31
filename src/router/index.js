56a0491326ad4ca69e6c2b4a6a5233d3f6040666
Mads Welling <54644486+MadsWAndersen@users.noreply.github.com>
2023-03-31 09:29:37 +0200

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import IndexView from "../views/IndexView.vue";
 import AboutView from "../views/AboutView.vue";
-import TestView from "../views/TestView.vue";
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
